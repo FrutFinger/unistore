d380b5f55d2cf7dc9864fcd2b4ec1649906cd1aa
Nagibator228proYT <bfbbfb65@gmail.com>
2025-06-23 05:39:29 +0300

    Add CSRF_TRUSTED_ORIGINS for Railway domain

--- config/settings.py
+++ config/settings.py
@@ -29,6 +29,11 @@ DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
+# Добавляем доверенный источник для CSRF
+CSRF_TRUSTED_ORIGINS = ['https://' + host for host in ALLOWED_HOSTS if host != 'localhost' and host != '127.0.0.1']
+if 'web-production-edc89.up.railway.app' not in ALLOWED_HOSTS:
+    CSRF_TRUSTED_ORIGINS.append('https://web-production-edc89.up.railway.app')
+
 
 # Application definition
 
