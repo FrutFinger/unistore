acabb10427ae8b4c95264279681a41014dc30228
Nagibator228proYT <bfbbfb65@gmail.com>
2025-06-23 06:10:59 +0300

    Fix duplicate ProductAdmin in admin.py

--- clotheshop/admin.py
+++ clotheshop/admin.py
@@ -19,25 +19,6 @@ class ProductSizeInline(admin.TabularInline):
     model = ProductSize
     extra = 1
 
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'product_type', 'get_stock_status')
-    list_filter = ('product_type',)
-    search_fields = ('name',)
-    inlines = [ProductSizeInline]
-    
-    def get_stock_status(self, obj):
-        """Show stock status in admin list"""
-        total_stock = sum(size.quantity for size in obj.sizes.all())
-        return f"В наличии: {total_stock}" if total_stock > 0 else "Нет в наличии"
-    
-    get_stock_status.short_description = 'Наличие'
-
-    class Media:
-        css = {
-            'all': ('admin/css/custom_admin.css',),
-        }
-
-
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     extra = 0
